e120e592c0c65336cb3bb2545053c9643720b3de
ABNER SILVA DE OLIVEIRA <abner.oliveira@serpro.gov.br>
2015-09-25 12:46:59 -0300

    added sourcemaps

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
+var fs = require('fs');
 
 var $ = require('gulp-load-plugins')();
 
@@ -18,8 +19,10 @@ gulp.task('typescript', [], function() {
     .pipe($.tslint.report('prose', {
       emitError: false
     }))
+    .pipe($.sourcemaps.init())
     .pipe($.typescript(tsProject)).on('error', conf.errorHandler(
       'TypeScript'))
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest('./src/'));
 });
 
@@ -28,10 +31,14 @@ gulp.task('browserify', ['typescript'], function() {
       read: false
     })
     .pipe($.browserify())
+    .pipe($.sourcemaps.init())
     .pipe($.rename('smartFactory.js'))
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest('./dist'))
+    .pipe($.sourcemaps.init())
     .pipe($.uglify())
     .pipe($.rename('smartFactory.min.js'))
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest('./dist'));
 
 });
--- src/configDecorator.js
+++ src/configDecorator.js
@@ -1,1 +1,3 @@
 
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJjb25maWdEZWNvcmF0b3IuanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
